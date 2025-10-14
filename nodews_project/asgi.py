5d2c1cc6653c5ca94f8a77245a0e161795271203
Pstas <ballabtnd@gmail.com>
2025-10-14 17:17:42 +0200

    daphne asgi fix

--- nodews_project/asgi.py
+++ nodews_project/asgi.py
@@ -9,13 +9,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from bufe.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nodews_project.settings')
 
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
+
+# Import these after get_asgi_application() to ensure apps are loaded
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from bufe.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
